faeb397a2ea5d2339b4e3f6a703ac697e7630f2d
Deborah Ho <debho@umich.edu>
2022-10-12 14:03:21 -0400

    oopsie i messed up

--- scripts/r/survival_plots.R
+++ scripts/r/survival_plots.R
@@ -2,21 +2,25 @@ library(ggplot2)
 library(paletteer)
 library(gtools)
 library(gridExtra)
+library(ggpubr)
 
 survival_residuals <- dat %>%
   filter(!is.na(growth_sc)) %>%
   mutate(survival_to_autumn_res = scale(predict(survival_to_autumn)),
-         survival_to_200d_res = scale(predict(survival_to_200d)))
+         survival_to_200d_res = scale(predict(survival_to_200d)), 
+         binned_density = factor(cut_interval(grid_density, n = 2),
+                                 labels = c("low", "high")))
 
 survival_autumn_oft_fig <- ggplot(survival_residuals,
-                                  aes(oft1, made_it, col = grid_density)) +
+                                  aes(oft1, made_it, col = binned_density)) +
   theme_classic() +
   geom_point(size = 3,
              alpha = 0.5) +
-  stat_smooth(aes(color = grid_density),
-              method = "lm",
-              se = F) +
-  scale_color_paletteer_c("viridis::viridis") + 
+  stat_smooth(aes(color = binned_density),
+              method = "glm",
+              se = F,
+              method.args = list(family = binomial)) +
+  scale_color_paletteer_d("colorBlindness::Blue2Gray8Steps") + 
   labs(x = "Activity",
        y = "Probability of survival to autumn",
        col = "Grid density (Spring)",
@@ -24,42 +28,47 @@ survival_autumn_oft_fig <- ggplot(survival_residuals,
 plot(survival_autumn_oft_fig)
 
 survival_autumn_mis_fig <- ggplot(survival_residuals,
-                                  aes(mis1, made_it, col = grid_density)) +
+                                  aes(mis1, made_it, col = binned_density)) +
   theme_classic() +
   geom_point(size = 3,
              alpha = 0.5) +
-  stat_smooth(aes(color = grid_density),
-              method = "lm",
-              se = F) +
-  scale_color_paletteer_c("viridis::viridis") + 
+  stat_smooth(aes(color = binned_density),
+              method = "glm",
+              se = F,
+              method.args = list(family = binomial)) +
+  scale_color_paletteer_d("colorBlindness::Blue2Gray8Steps") + 
   labs(x = "Aggression",
        y = "Probability of survival to autumn",
+       col = "Grid density (Spring)",
        tag = "b.")
 plot(survival_autumn_mis_fig)
 
 survival_200d_oft_fig <- ggplot(survival_residuals,
-                                  aes(oft1, survived_200d, col = grid_density)) +
+                                  aes(oft1, survived_200d, col = binned_density)) +
   theme_classic() +
   geom_point(size = 3,
              alpha = 0.5) +
-  stat_smooth(aes(color = grid_density),
-              method = "lm",
-              se = F) +
-  scale_color_paletteer_c("viridis::plasma") + 
+  stat_smooth(aes(color = binned_density),
+              method = "glm",
+              se = F,
+              method.args = list(family = binomial)) +
+  scale_color_paletteer_d("colorBlindness::Blue2Orange8Steps") + 
   labs(x = "Activity",
        y = "Probability of survival to 200 days",
+       col = "Grid density (Spring)",
        tag = "c.")
 plot(survival_200d_oft_fig)
 
 survival_200d_mis_fig <- ggplot(survival_residuals,
-                                  aes(mis1, survived_200d, col = grid_density)) +
+                                  aes(mis1, survived_200d, col = binned_density)) +
   theme_classic() +
   geom_point(size = 3,
              alpha = 0.5) +
-  stat_smooth(aes(color = grid_density),
-              method = "lm",
-              se = F) +
-  scale_color_paletteer_c("viridis::plasma") + 
+  stat_smooth(aes(color = binned_density),
+              method = "glm",
+              se = F,
+              method.args = list(family = binomial)) +
+  scale_color_paletteer_d("colorBlindness::Blue2Orange8Steps") + 
   labs(x = "Aggression",
        y = "Probability of survival to 200 days",
        col = "Grid density (Spring)",
